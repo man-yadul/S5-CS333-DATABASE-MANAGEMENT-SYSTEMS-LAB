e14118779942b14cd4badcc65afb54de473d375e
yadul <95083929+man-yadul@users.noreply.github.com>
2021-12-13 22:24:39 +0530

    Update Day - 2.sql

--- Day - 2.sql
+++ Day - 2.sql
@@ -1,3 +1,51 @@
+/*
+Performing DML commands like Insertion, Deletion, Modifying, Altering, and Updating records based on conditions.
+a. Create and insert the following values in Employee table.
+EmpId EmpName Job Manager DeptNo HireDate Salary Comm
+E0001 Abey Tester E0004 D004 15/12/12 30000 500
+E0002 Jesto Analyst E0001 D002 24/04/11 25000 650
+E0003 Adarsh Clerk E0004 D004 10/01/13 15000 500
+E0004 Kevin Admin E0005 D002 10/10/13 20000 1750
+E0005 Bony Manager D001 11/04/11 50000 1000
+E0006 Manu Supplier E0001 D003 19/06/13 5000 450
+b. Create and insert the following values in Department table.
+DeptNo DeptName Location
+D001 DeptA A block
+D002 DeptB B block
+D003 DeptC C block
+D004 DeptD D block
+c. Create a table as shown below and insert the values:
+Table Name: Client
+Name Data type
+Cno Varchar2(6)
+Name Varchar2(20)
+City Varchar2(15)
+PinCode Number(8)
+State Varchar2(15)
+Baldue Number(10,2)
+d. Insert the following values into the Client table
+Cno Name City Pincode State Baldue
+C01 Nimmy Cochin 680324 Kerala 14000
+C02 Susan Cochin 680324 Kerala 0
+C03 Anjana Salem 680345 TamilNadu 5000
+C04 Sneha Pune 680223 Maharashtra 500
+C05 Annet Gandipuram 680321 Karnataka 7600
+C06 Ann Bombay 682111 Maharashtra 2055
+e. Display the entire contents of the tables.
+f. Insert a new employee with EmpId=E0001.
+g. Display the first 4 rows of the Client table.
+h. Retrieve the names, Hire date and Salary of all employees.
+i. List all employees whose name starts with ‘A’ or ‘K’.
+j. Display the distinct cities to which the clients belong.
+k. List managers hired after 2012 and whose salary is more than 17500.
+l. List all employees whose name falls in the alphabetical range ‘E to L’.
+m. Change the city of Client No: C03 from ‘Salem’ to ‘Delhi’.
+n. Change the Commission of employee ‘Abey’ to 600.
+o. List all from Client table and display in the following format: “Annet from Gandhipuram has 7600 Balance due”.
+p. Display the column name ‘Baldue’ of Client table as “Due Balance”
+q. List all clients who are not from ‘Kerala’.
+*/
+
 a.  CREATE TABLE Employee (
     EmpId Varchar2(5) NOT NULL PRIMARY KEY,
     EmpName Varchar2(20),
