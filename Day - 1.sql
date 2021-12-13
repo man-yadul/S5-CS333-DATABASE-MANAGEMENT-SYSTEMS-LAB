aa63750f163be98e9a607e1fe5105453a25f2f6a
yadul <95083929+man-yadul@users.noreply.github.com>
2021-12-13 22:24:07 +0530

    Update Day - 1.sql

--- Day - 1.sql
+++ Day - 1.sql
@@ -1,3 +1,31 @@
+/*
+Creation of a database using DDL commands and writes DQL queries to retrieve information from the database.
+a. Create the following tables.
+i. Table Name: Hospital
+Column Name Data Type Constraint
+DoctorId Varchar2(10) Primary Key
+DocName Varchar2(20)
+Specialization Varchar2(10)
+HOD Varchar2(10)
+DeptNo Varchar2(5)
+HireDate Date
+Sal Number(10,3)
+ii. Table Name: HospitalDept
+Column Name Data Type Constraint
+DeptNo Varchar2 (5) Primary key
+DeptName Varchar2(20)
+Location Varchar2(20)
+b. Add a column “Additional Qualification”to the Hospital table. The data type is character.
+c. Modify the column width of the Specialization field of Hospital table.
+d. Remove column Location from HospitalDept table.
+e. Rename the column name Sal to Salary in Hospital table.
+f. Delete the table HospitalDept.
+g. Display your name and age using DUAL table.
+h. Display the current system date.
+i. Perform the following operations: 55-15+8*3/6
+j. Display all the tables currently available to the user.
+*/
+
 a.  (i)     CREATE TABLE Hospital (
             DoctorId Varchar2(10) PRIMARY KEY,
             DocName Varchar2(20),
@@ -39,4 +67,3 @@ i.  SELECT 55-15+8*3/6
 
 j.  SELECT table_name 
     FROM user_tables;
-
