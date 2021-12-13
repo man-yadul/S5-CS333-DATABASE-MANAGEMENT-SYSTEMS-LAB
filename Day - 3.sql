5c65c60b12e6d26df1bc82a83cf7fa476741e90b
yadul <95083929+man-yadul@users.noreply.github.com>
2021-12-13 22:26:27 +0530

    Update Day - 3.sql

--- Day - 3.sql
+++ Day - 3.sql
@@ -1,3 +1,16 @@
+/*
+3. Creating relationship between the databases.
+3a. Alter the client table to add Primary key constraint on Cno Column.
+3b. Create foreign key constraint on Manager and Dept. No column of Employee table.
+3c. Insert a new employee with DeptNo as D006.
+4. Creating a database to set various constraints.
+4a. Add a constraint to make the Job column unique.
+4b. Add constraint to the Employee table to check if Comm> 400.
+4c. Set Salary value by default as 25000, otherwise as the user enters.
+4d. Remove the constraint from the column Job.
+4e. Display all the constraints on the Employee table.
+*/
+
 3a.
 ALTER TABLE Client
 ADD PRIMARY KEY (Cno);
