e29cc9f7b5afc8d8cc420fef37151f712be2c14d
yadul <95083929+man-yadul@users.noreply.github.com>
2021-12-18 10:36:44 +0530

    Create Day - 6.sql

---
+++ Day - 6.sql
@@ -0,0 +1,16 @@
+/*
+Implementation of Order By, Group by & Having clause. *
+a. Display the client names in ascending order.
+b. Determine the maximum salary in each department.
+c. Display the minimum salary of all departments except D001.
+Implementation of set operators, nested queries and Join queries *
+a. Display all the DeptNo available with the dept and employee tables.
+b. Display all the DeptNoavailable in employee and not in dept tables.
+c. Write a nested query to select the employees who have got more salary than Adarsh.
+d. Write a nested query to select the employees who have less salary than any employees working in dept D004.
+e. Display all employee names and salary whose salary is greater than minimum salary of the company and job title starts with A.
+f. Display Employee Name and Department Name of employees whose salary is greater than 20000.
+g. Display the Employee name, Job, Manager Name by implementing a left outer join.
+h. Display the Employee name, Job, Manager Name by implementing a right outer join.
+i. Display the Employee name, Job, Manager Name by implementing a full outer join.
+*/
