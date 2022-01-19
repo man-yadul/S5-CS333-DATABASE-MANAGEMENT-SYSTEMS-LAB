a13d047f0a42bf9c716f3387034379ae3d58b1dc
yadul <95083929+man-yadul@users.noreply.github.com>
2022-01-19 10:41:45 +0530

    Update Day - 7.sql

--- Day - 7.sql
+++ Day - 7.sql
@@ -1,3 +1,15 @@
+/*
+12. Implementation of various control structures using PL/SQL * 
+a. Write a PL/SQL code block to calculate the area of a circle for a value of radius 
+varying from 5 to 15. Store the radius and the corresponding values of 
+calculated area in an empty table named areas, consisting of two columns 
+radius & area. 
+b. Write a PL/SQL code block that will accept an account number from the user, 
+check if the users balance is less than minimum balance, then 
+deduct Rs.100/ -from the balance. This process is fired on the ACCOUNT table. 
+(Exception handling in PL/SQL) 
+*/
+
 12a.
 CREATE TABLE AREAS
 (
