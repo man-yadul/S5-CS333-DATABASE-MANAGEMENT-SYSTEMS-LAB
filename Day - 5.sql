ba331104f1fc8b5b1d57636d366bf289ac776e15
yadul <95083929+man-yadul@users.noreply.github.com>
2022-01-19 10:54:10 +0530

    Update Day - 5.sql

--- Day - 5.sql
+++ Day - 5.sql
@@ -25,8 +25,7 @@ Manager
 9d. Find the average salary in Employee table. 
 */
 
-7.
-7a.
+-- 7a.
 CREATE VIEW CUST_VW AS 
 SELECT EmpId, EmpName, Job, Manager
 FROM Employee;
@@ -47,72 +46,70 @@ Enter password: ********
 Connected.
 */
 
-7b.
+-- 7b.
 UPDATE CUST_VW
 SET Job = 'Senior Clerk' 
 WHERE Job = 'Clerk';
 
-7c.
+-- 7c.
 ALTER TABLE Employee
 DROP CONSTRAINT FK_EMP;
 
 DELETE FROM CUST_VW
 WHERE EmpName = 'Kevin';
 
-7d.
+-- 7d.
 DROP VIEW CUST_VW;
 
-8.
-8a.
+-- 8a.
 SELECT POWER(16, 5)
 FROM DUAL;
 
-8b.
+-- 8b.
 SELECT LENGTH('Application')
 FROM DUAL;
 
-8c.
+-- 8c.
 SELECT LAST_DAY(SYSDATE)
 FROM DUAL;
 
-8d.
+-- 8d.
 SELECT TO_CHAR(65432,'$99999') 
 FROM DUAL;
 
-8e.
+-- 8e.
 SELECT SYSTIMESTAMP 
 FROM DUAL;
 
-8f.
+-- 8f.
 SELECT TO_CHAR(HireDate, 'YYYY') HireDate 
 FROM Employee;
 
-8g.
+-- 8g.
 SELECT TRUNC(MONTHS_BETWEEN(SYSDATE, TO_DATE(HireDate))) 
 AS HireDate 
 FROM Employee;
 
-8h.
+-- 8h.
 SELECT COUNT(DISTINCT(DeptName)) 
 FROM Department;
 
-8i.
+-- 8i.
 SELECT TO_CHAR(HireDate, 'DY, DDth MONTH, YYYY') 
 FROM Employee;
 
-9.
-9a.
+-- 9a.
 SELECT SUM(Baldue)
 FROM Client;
 
-9b.
+-- 9b.
 SELECT COUNT(*) 
 FROM Employee;
 
-9c.
+-- 9c.
 SELECT MIN(Baldue), MAX(Baldue) 
 FROM Client;
 
-9d.
+-- 9d.
 SELECT AVG(Salary) 
 FROM Employee;
